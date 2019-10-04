5148b9417135038b77cf849d3c8a2562909481a1
beverlyosoria <bevosoria@gmail.com>
2019-10-04 15:03:34 -0500

    creates sign up login pages

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Deafinitely Friendly</h1>
       </header>
+      
     </div>
   );
 }
