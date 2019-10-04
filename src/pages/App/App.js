c5ecb2e44e1ea743883b046706d47b8ebde9a1d8
beverlyosoria <bevosoria@gmail.com>
2019-10-04 16:53:45 -0500

    Refactor the server to hash the password when a user signs up

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,65 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+import SignupPage from "../SignupPage/SignupPage";
+import LoginPage from "../LoginPage/LoginPage";
+import userService from "../../utils/userService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Deafinitely Friendly</h1>
-      </header>
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: userService.getUser()
+    };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  };
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() });
+  };
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <h1>App</h1>
+
+          <br />
+          <Link to="/signup">Signup</Link>
+          <br />
+          <Link to="/login">Login</Link>
+
+          <Route
+            exact
+            path="/signup"
+            render={({ history }) => (
+              <SignupPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={({ history }) => (
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
+          />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
