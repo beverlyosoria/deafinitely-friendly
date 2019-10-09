08f1327abe7e21e5bb164fd1402e789e3eabd03c
beverlyosoria <bevosoria@gmail.com>
2019-10-09 14:32:45 -0500

    new post changes

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,42 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './NewPost.css';
 import Map from './../../components/Map/Map';
 
 class NewPost extends Component {
-    state = {
-    name: ''
-    }
+	state = {
+		name: ''
+	};
 
-handleOnChange = event => {
-    this.setState({
-        name: event.target.value
-    })
-}
+	handleOnChange = (event) => {
+		this.setState({
+			name: event.target.value
+		});
+	};
 
-
-render() {
-    return (
-    <div>
-    <h1>Post</h1>
-    <form className="post" >
-            {' '}
-            
-            <input type="text" name="name" onChange={this.handleOnChange} value={this.state.name} />
-            <Map
-            handleAddPost={this.props.handleAddPost}
-            name={this.state.name}
-     google={this.props.google}
-     center={{lat: 30.2672, lng: -97.7431}}
-     height='300px'
-     zoom={15}
-    />
-    <br />
-    <br />
-    <br />
-    
-    </form>
-    </div>
-    )}
+	render() {
+		return (
+			<div>
+				<h1>Post</h1>
+				<form className="post">
+					{' '}
+					<input type="text" name="name" onChange={this.handleOnChange} value={this.state.name} />
+					<Map
+						handleAddPost={this.props.handleAddPost}
+						name={this.state.name}
+						google={this.props.google}
+						center={{ lat: 30.2672, lng: -97.7431 }}
+						height="300px"
+						zoom={15}
+					/>
+					<br />
+					<br />
+					<br />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default NewPost;
